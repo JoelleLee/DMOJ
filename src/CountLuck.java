03f6d6af678b32cc7d6d12085cfa92c10c541743
JoelleLee <90375161+JoelleLee@users.noreply.github.com>
2022-04-03 01:34:45 -0400

    Hacker Rank problem, worked on writing clean code and efficiency of program. Work on terminating cases in recursive functions

--- src/CountLuck.java
+++ src/CountLuck.java
@@ -2,53 +2,57 @@ import java.io.*;
 
 public class CountLuck {
     //north0   east1    south2    west3
-    public static void direct(int[][] map, int[] rc, int[] directions){
+    public static int direct(int[][] map, int r, int c){
+        int paths = 0;
 
-        if(rc[0]-1>=0 && map[rc[0]-1][rc[1]]==0){ //north
-            directions[0] = 1;
-        }
-        else{
-            directions[0] = 0;
+        if(r-1>=0 && (map[r-1][c]==1 || map[r-1][c]==2)){ //north
+            paths++;
         }
 
-        if(rc[1]+1<map[0].length && map[rc[0]][rc[1]+1]==0){ //east
-            directions[1] = 1;
-        }
-        else{
-            directions[1] = 0;
+        if(c+1<map[0].length && (map[r][c+1]==1|| map[r][c+1]==2)){ //east
+            paths++;
         }
 
-        if(rc[0]+1<map.length && map[rc[0]+1][rc[1]]==0){ //south
-            directions[2] = 1;
-        }
-        else{
-            directions[2] = 0;
+        if(r+1<map.length && (map[r+1][c]==1 || map[r+1][c]==2)){ //south
+            paths++;
         }
 
-        if(rc[1]-1>=0 && map[rc[0]][rc[1]-1]==0){ //west
-            directions[3] = 1;
+        if(c-1>=0 && (map[r][c-1]==1 || map[r][c-1]==2)){ //west
+            paths++;
         }
-        else{
-            directions[3] = 0;
-        }
+
+        return paths;
     }
-    public static int luck(int[][] map, int[] rc, int[] direct, int turns){
-
-        direct(map, rc, direct);
-
-        if(rc[0]>=map.length || rc[1]>=map[0].length || rc[0]<0 || rc[1]<0 || direct[0]+direct[1]+direct[2]+direct[3]==0){
-            return -1;
+    public static boolean luck(int[][] map, int guess, int r, int c, int turns){//edit
+        if(r<0|| c<0 || r>=map.length || c>=map[0].length || map[r][c]<1 || turns>guess){
+            return false;
         }
 
-        if(map[rc[0]][rc[1]]==2){
-            return turns;
+        if(map[r][c]==2){
+            return guess==turns;
         }
 
-        for(int i=0; i<4; i++){
-
+        int ways = direct(map, r, c);
+        if(ways<=0){
+            return false;
         }
-        return 0;
+        else if(ways>1){
+            turns++;
+        }
 
+        map[r][c] = -1;
+
+        for(int i=-1; i<=1; i++){
+            for(int j=-1; j<=1; j++){
+                if(!(i==0 && j==0) && luck(map, guess,r+i, c+j, turns)){
+                    return true;
+                }
+            }
+        }
+
+        map[r][c]=1;
+
+        return false;
     }
     public static void main(String[] args) throws IOException{
         DataInputStream stdin = new DataInputStream(System.in);
@@ -61,36 +65,40 @@ public class CountLuck {
             int col = Integer.parseInt(input[1]);
             int[][] map = new int[row][col];
 
-            int[] rc = new int[2];
+            int rP = 0;
+            int cP = 0;
 
-            for(int j=0; j<map.length; j++){
-                input = stdin.readLine().split(" ");
-                for(int k=0; k<map[0].length; k++){
-                    //.=0     X=-1      prev=-1   *=2
+            for(int j=0; j<row; j++){
+                input = stdin.readLine().split("");
+                for(int k=0; k<col; k++){
+                    //.=1     X=-1      prev=-1   *=2
                     if(input[k].equals("X")){
                         map[j][k] = -1;
                     }
+                    else if(input[k].equals(".")){
+                        map[j][k] = 1;
+                    }
                     else if(input[k].equals("M")){
-                        rc[0] = j;
-                        rc[1] = k;
-                        map[j][k] = 0;
+                        rP = j;
+                        cP = k;
+                        map[j][k] = 1;
                     }
                     else if(input[k].equals("*")){
                         map[j][k]=2;
                     }
-                    else{
-                        map[j][k] = 0;
-                    }
-                    map[j][k] = Integer.parseInt(input[k]);
                 }
             }
 
             int guess = Integer.parseInt(stdin.readLine());
-            boolean[] directions = new boolean[4];
 
-            System.out.println(luck(map, rc, new int[4], 0));
-
+            if(luck(map, guess, rP, cP, 0)){
+                System.out.println("Impressed");
+            }
+            else{
+                System.out.println("Oops!");
+            }
         }
 
     }
 }
+
